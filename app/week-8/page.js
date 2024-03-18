7c4e3d5141a3b1e4854a7958f8c4efe394f5bab4
thuymy.lam <thuymylam96@gmail.com>
2024-03-17 18:15:56 -0600

    Update Week 8 page.js

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -1,12 +1,7 @@
 "use client";
-// Import the useUserAuth hook
-//import { useUserAuth } from "./_utils/auth-context";
 import SignIn from "./_utils/sign-in";
 
 export default function Page() {
- 
-// Use the useUserAuth hook to get the user object and the login and logout functions
-//const { user } = useUserAuth();
 
 return (
     <div className="m-2 p-2">
@@ -15,29 +10,3 @@ return (
     </div>
 );
 }
-
-// "use client";
-
-// import { useUserAuth } from "./_utils/auth-context";
-
-// export default function Page() {
-//   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
-
-//   console.log(user);
-
-//   return (
-//     <div>
-//       <h1>Week 8</h1>
-//       <p>{user ? "Hi there!" : "Please sign in"}</p>
-//       <p>{user?.email}</p>
-//       {user && user.displayName}
-//       <p>
-//         {user ? (
-//           <button onClick={firebaseSignOut}>Sign Out</button>
-//         ) : (
-//           <button onClick={gitHubSignIn}>Sign In with GitHub</button>
-//         )}
-//       </p>
-//     </div>
-//   );
-// }
